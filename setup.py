a800ddb7e99d477f604a030aef153f4ac93cc0a0
Gavin Hu <huhz1986@gmail.com>
2021-12-22 23:17:27 +0800

    release 1.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ project_dir = Path(__file__).parent
 
 setuptools.setup(
     name="mysqlog",
-    version="1.0.4",
+    version="1.0.5",
     description="MySQL Log",
     # Use UTF-8 encoding for README even on Windows by using the encoding argument.
     long_description=project_dir.joinpath("README.md").read_text(encoding="utf-8"),
